42d15c1c5f070e821b68f73fae98ee7ec81e7871
cvera-tech <25487412+cvera-tech@users.noreply.github.com>
2024-01-25 16:42:33 -0500

    Add comments for Collection.Card fields

--- server/Models/collection/Card.cs
+++ server/Models/collection/Card.cs
@@ -3,6 +3,12 @@ namespace CardboardArchivistAPI.Models.Collection;
 public class Card
 {
     public required string UUID { get; set; }
+
+    // The UUID of the card in the reference database. This is where we get all the card's info such as the name, rules text, set info, etc.
     public required string ReferenceUUID { get; set; }
+
+    // The Deck to which this card belongs. Each card can only belong to one deck. If null, the card is in the general collection.
     public string? DeckUUID { get; set; }
+
+    // TODO: Add support for card condition (Mint, Near Mint, Excellent, Good, Damaged, etc.)
 }
