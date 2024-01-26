a501e968ed0c3450ea8578501ad37c31bb230f4f
cvera-tech <25487412+cvera-tech@users.noreply.github.com>
2024-01-26 17:07:42 -0500

    Add ICardService to service container

--- server/Program.cs
+++ server/Program.cs
@@ -1,9 +1,14 @@
+using CardboardArchivistAPI.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Dependency injection
+builder.Services.AddScoped<ICardService, InMemoryCardService>();
 
 var app = builder.Build();
 
@@ -15,6 +20,12 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.MapGet("/", (ICardService _cardService) =>
+{
+    return _cardService.GetCards();
+}
+);
 
 var summaries = new[]
 {
